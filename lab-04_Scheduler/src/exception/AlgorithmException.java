77a46b776c657c9861e8bd0b5841bf7872b01c79
Jarelio Filho <jareliofilho@gmail.com>
2021-01-29 23:20:50 -0300

    Add algorithms examples on algorithm exception string

--- lab-04_Scheduler/src/exception/AlgorithmException.java
+++ lab-04_Scheduler/src/exception/AlgorithmException.java
@@ -3,7 +3,7 @@ package exception;
 public class AlgorithmException extends RuntimeException {
 
     public AlgorithmException() {
-        super("The referred algorithm is invalid, you must use: ");
+        super("The referred algorithm is invalid, you must use: Algorithms: FCFS, SJF, SJFP, Priority, PriorityP and RR");
     }
 
     public AlgorithmException(String message) {
