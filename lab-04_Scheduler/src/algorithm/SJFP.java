620059ca714751885e8f4fe218d7965bdea84cec
Tibet Teixeira <tibetbrasil@gmail.com>
2021-01-30 03:07:56 -0300

    It adds SJF Preemptive algorithm

--- lab-04_Scheduler/src/algorithm/SJFP.java
+++ lab-04_Scheduler/src/algorithm/SJFP.java
@@ -1,4 +1,74 @@
 package algorithm;
 
+import model.BCP;
+import model.Process;
+import model.queue.Queue;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class SJFP extends Algorithm {
+    private BCP getShortestJob(Queue readyQueue, int currentUnitTime) {
+        Predicate<BCP> byArrivalTime = bcp -> bcp.getProcess().getArrivalTime() <= currentUnitTime;
+        List<BCP> bcps = readyQueue.getAll().stream().filter(byArrivalTime).collect(Collectors.toList());
+
+        if (bcps.size() == 0) {
+            return null;
+        }
+
+        if (bcps.size() == 1) {
+            return bcps.get(0);
+        }
+
+        bcps.sort(Comparator.comparingInt(bcp -> (bcp.getProcess().getBurstTime() - bcp.getTotalBurstExecuted())));
+
+        return bcps.get(0);
+    }
+
+    @Override
+    public void run(Queue readyQueue, Queue runningQueue, Queue terminatedQueue) {
+        BCP bcp;
+        Process process;
+        int currentUnitTime = 0;
+        int waitingTime;
+
+        final int CPU_TIME_EXECUTED = 1;
+
+        while (readyQueue.length() > 0) {
+            bcp = getShortestJob(readyQueue, currentUnitTime);
+
+            if (bcp == null) {
+                currentUnitTime += 1;
+                continue;
+            } else {
+                readyQueue.remove(bcp);
+            }
+
+            process = bcp.getProcess();
+            runningQueue.add(bcp);
+
+            waitingTime = currentUnitTime - bcp.getLastUnitTimeExecuted();
+
+            if (bcp.isFirstExecuted()) {
+                waitingTime -= process.getArrivalTime();
+                bcp.setFirstExecuted(false);
+            }
+
+            bcp.setTotalWaitingTime(bcp.getTotalWaitingTime() + waitingTime);
+            bcp.setTotalBurstExecuted(bcp.getTotalBurstExecuted() + CPU_TIME_EXECUTED);
+            bcp.setTurnaroundTime(bcp.getTotalWaitingTime() + bcp.getTotalBurstExecuted());
+            currentUnitTime += CPU_TIME_EXECUTED;
+            bcp.setLastUnitTimeExecuted(currentUnitTime);
+
+            bcp = runningQueue.pop();
+
+            if (bcp.getTotalBurstExecuted() == process.getBurstTime()) {
+                terminatedQueue.add(bcp);
+            } else {
+                readyQueue.add(bcp);
+            }
+        }
+    }
 }
