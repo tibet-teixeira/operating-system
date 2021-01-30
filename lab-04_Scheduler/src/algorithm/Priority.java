81a7568099a399aac6f0a91bb3763b3e958f36b4
Tibet Teixeira <tibetbrasil@gmail.com>
2021-01-30 04:10:16 -0300

    It adds Priority Non-Preemptive algorithm

--- lab-04_Scheduler/src/algorithm/Priority.java
+++ lab-04_Scheduler/src/algorithm/Priority.java
@@ -1,4 +1,62 @@
 package algorithm;
 
+import model.BCP;
+import model.Process;
+import model.queue.Queue;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Priority extends Algorithm {
+    private BCP getShortestJob(Queue readyQueue, int currentUnitTime) {
+        Predicate<BCP> byArrivalTime = bcp -> bcp.getProcess().getArrivalTime() <= currentUnitTime;
+        List<BCP> bcps = readyQueue.getAll().stream().filter(byArrivalTime).collect(Collectors.toList());
+
+        if (bcps.size() == 0) {
+            return null;
+        }
+
+        if (bcps.size() == 1) {
+            return bcps.get(0);
+        }
+
+        bcps.sort(Comparator.comparingInt(bcp -> bcp.getProcess().getPriority()));
+
+        return bcps.get(0);
+    }
+
+    @Override
+    public void run(Queue readyQueue, Queue runningQueue, Queue terminatedQueue) {
+        BCP bcp;
+        Process process;
+        int currentUnitTime = 0;
+        int waitingTime;
+
+        while (readyQueue.length() > 0) {
+            bcp = getShortestJob(readyQueue, currentUnitTime);
+
+            if (bcp == null) {
+                currentUnitTime += 1;
+                continue;
+            } else {
+                readyQueue.remove(bcp);
+
+                process = bcp.getProcess();
+                runningQueue.add(bcp);
+
+                waitingTime = Math.max(currentUnitTime - process.getArrivalTime(), 0);
+                bcp.setTotalWaitingTime(waitingTime);
+                bcp.setTotalBurstExecuted(process.getBurstTime());
+                bcp.setTurnaroundTime(waitingTime + process.getBurstTime());
+                currentUnitTime += process.getBurstTime();
+                bcp.setLastUnitTimeExecuted(currentUnitTime);
+
+                bcp = runningQueue.pop();
+                terminatedQueue.add(bcp);
+            }
+        }
+    }
 }
